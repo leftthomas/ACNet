4c393934561ae7742167d7fc4412dce4e4632a73
leftthomas <leftthomas@qq.com>
2021-09-20 18:31:31 +0800

    change in channel of G and D to 8

--- train.py
+++ train.py
@@ -148,8 +148,8 @@ if __name__ == '__main__':
 
     # model define
     extractor = Extractor(backbone_type, emb_dim).cuda()
-    generator = Generator(in_channels=16).cuda()
-    discriminator = Discriminator(in_channels=16).cuda()
+    generator = Generator(in_channels=8).cuda()
+    discriminator = Discriminator(in_channels=8).cuda()
 
     # loss setup
     class_criterion = NormalizedSoftmaxLoss(len(train_data.classes), emb_dim).cuda()
