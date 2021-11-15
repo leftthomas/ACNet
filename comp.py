8447ae49ef9cdb1f64bd9ef0aa31b4f95dd30674
leftthomas <leftthomas@qq.com>
2021-11-15 18:45:01 +0800

    add code to vis baseline vectors

--- comp.py
+++ comp.py
@@ -1,6 +1,6 @@
 import argparse
 import glob
-import os
+import random
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -8,7 +8,6 @@ import pandas as pd
 import seaborn as sns
 import torch
 from PIL import Image
-from matplotlib.lines import Line2D
 from sklearn.manifold import TSNE
 from tqdm import tqdm
 
@@ -16,68 +15,143 @@ from model import Extractor
 from utils import get_transform
 
 
-def draw_fig(vectors, legends, save_path, style=None):
+def draw_fig(vectors, legends, style, ax, legend_on=False):
     x_min, x_max = np.min(vectors, 0), np.max(vectors, 0)
     vectors = (vectors - x_min) / (x_max - x_min)
-    if style is not None:
-        data = pd.DataFrame({'x': vectors[:, 0].tolist(), 'y': vectors[:, 1].tolist(),
-                             'label': legends, 'domain': style})
-        sns.scatterplot(x='x', y='y', hue='label', style='domain', palette='Set2', data=data)
+    data = pd.DataFrame({'x': vectors[:, 0].tolist(), 'y': vectors[:, 1].tolist(), 'label': legends, 'domain': style})
+    if legend_on:
+        sns.scatterplot(x='x', y='y', hue='label', style='domain', palette='Set2', data=data, ax=ax)
+        ax.legend(loc=2, bbox_to_anchor=(1.05, 1.0), borderaxespad=0.)
     else:
-        data = pd.DataFrame({'x': vectors[:, 0].tolist(), 'y': vectors[:, 1].tolist(), 'label': legends})
-        sns.scatterplot(x='x', y='y', hue='label', palette='Set2', data=data)
-    os.makedirs(os.path.dirname(save_path), exist_ok=True)
-    custom = [Line2D([], [], marker='.', color='b', linestyle='None'),
-              Line2D([], [], marker='.', color='r', linestyle='None')]
-    plt.legend(custom, ['Yes', 'No'], loc='lower right')
-    plt.savefig(save_path, bbox_inches='tight', pad_inches=0.1)
-    plt.clf()
+        sns.scatterplot(x='x', y='y', hue='label', style='domain', palette='Set2', data=data, ax=ax, legend=False)
+    ax.set(xlabel=None)
+    ax.set(ylabel=None)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Vis Embedding Dist.')
-    parser.add_argument('--data_root', default='/data', type=str, help='Datasets root path')
     parser.add_argument('--data_name', default='sketchy', type=str, choices=['sketchy', 'tuberlin'],
                         help='Dataset name')
-    parser.add_argument('--num_class', default=5, type=int, help='Vis class number')
+    parser.add_argument('--num_class', default=10, type=int, help='Vis class number')
     parser.add_argument('--num_sample', default=20, type=int, help='Vis sample number')
     parser.add_argument('--save_root', default='result', type=str, help='Result saved root path')
-    parser.add_argument('--load_model', default='result/norm/sketchy_resnet50_512_extractor.pth', type=str,
-                        help='Loaded model to vis dist.')
 
     args = parser.parse_args()
-    data_root, data_name, num_class, num_sample = args.data_root, args.data_name, args.num_class, args.num_sample
-    save_root, load_model = args.save_root, args.load_model
+    data_name, num_class, num_sample, save_root = args.data_name, args.num_class, args.num_sample, args.save_root
 
-    model = Extractor(backbone_type='resnet50', emb_dim=512)
-    model.load_state_dict(torch.load(load_model, map_location='cpu'))
-    model = model.cuda()
-    model.eval()
+    norm_path = 'result/norm/{}_resnet50_512_extractor.pth'.format(data_name)
+    norm_model = Extractor(backbone_type='resnet50', emb_dim=512)
+    norm_model.load_state_dict(torch.load(norm_path, map_location='cpu'))
+    norm_model = norm_model.cuda()
+    norm_model.eval()
 
+    norm_gan_path = 'result/norm_gan/{}_resnet50_512_extractor.pth'.format(data_name)
+    norm_gan_model = Extractor(backbone_type='resnet50', emb_dim=512)
+    norm_gan_model.load_state_dict(torch.load(norm_gan_path, map_location='cpu'))
+    norm_gan_model = norm_gan_model.cuda()
+    norm_gan_model.eval()
+
+    triplet_path = 'result/triplet/{}_resnet50_512_extractor.pth'.format(data_name)
+    triplet_model = Extractor(backbone_type='resnet50', emb_dim=512)
+    triplet_model.load_state_dict(torch.load(triplet_path, map_location='cpu'))
+    triplet_model = triplet_model.cuda()
+    triplet_model.eval()
+
+    triplet_gan_path = 'result/triplet_gan/{}_resnet50_512_extractor.pth'.format(data_name)
+    triplet_gan_model = Extractor(backbone_type='resnet50', emb_dim=512)
+    triplet_gan_model.load_state_dict(torch.load(triplet_gan_path, map_location='cpu'))
+    triplet_gan_model = triplet_gan_model.cuda()
+    triplet_gan_model.eval()
+
+    random.seed(1)
     tsne = TSNE(n_components=2, init='pca', random_state=0)
 
     sketch_paths, photo_paths, sketch_labels, photo_labels = [], [], [], []
-    for class_name in sorted(glob.glob('/data/{}/val/sketch/*'.format(data_name)))[:num_class]:
+    for class_name in random.sample(sorted(glob.glob('/data/{}/val/sketch/*'.format(data_name))), k=num_class):
         class_name = class_name.split('/')[-1]
-        sketch_paths += sorted(glob.glob('/data/{}/val/sketch/{}/*'.format(data_name, class_name)))[:num_sample]
-        photo_paths += sorted(glob.glob('/data/{}/val/photo/{}/*'.format(data_name, class_name)))[:num_sample]
+        sketches = sorted(glob.glob('/data/{}/val/sketch/{}/*'.format(data_name, class_name)))
+        sketch_paths += random.sample(sketches, k=num_sample)
+        photos = sorted(glob.glob('/data/{}/val/photo/{}/*'.format(data_name, class_name)))
+        photo_paths += random.sample(photos, k=num_sample)
         sketch_labels += [class_name] * num_sample
         photo_labels += [class_name] * num_sample
+
+    sketch_gan_paths, photo_gan_paths, sketch_gan_labels, photo_gan_labels = [], [], [], []
+    for class_name in random.sample(sorted(glob.glob('/home/rh/Downloads/{}/val/sketch/*'.format(data_name))),
+                                    k=num_class):
+        class_name = class_name.split('/')[-1]
+        sketches = sorted(glob.glob('/home/rh/Downloads/{}/val/sketch/{}/*_fake.png'.format(data_name, class_name)))
+        sketch_gan_paths += random.sample(sketches, k=num_sample)
+        photos = sorted(glob.glob('/home/rh/Downloads/{}/val/photo/{}/*_real.png'.format(data_name, class_name)))
+        photo_gan_paths += random.sample(photos, k=num_sample)
+        sketch_gan_labels += [class_name] * num_sample
+        photo_gan_labels += [class_name] * num_sample
+
     transform = get_transform(split='val')
 
-    sketches, photos = [], []
-    for paths, embeds in zip([sketch_paths, photo_paths], [sketches, photos]):
+    norm_sketches, norm_photos = [], []
+    for paths, embeds in zip([sketch_paths, photo_paths], [norm_sketches, norm_photos]):
         for path in tqdm(paths, desc='processing data'):
             emd = transform(Image.open(path)).unsqueeze(dim=0)
             with torch.no_grad():
-                embeds.append(model(emd.cuda()).squeeze(dim=0).cpu())
+                embeds.append(norm_model(emd.cuda()).squeeze(dim=0).cpu())
 
-    sketches = torch.stack(sketches)
-    photos = torch.stack(photos)
-    sketches = tsne.fit_transform(sketches.numpy())
-    photos = tsne.fit_transform(photos.numpy())
+    triplet_sketches, triplet_photos = [], []
+    for paths, embeds in zip([sketch_paths, photo_paths], [triplet_sketches, triplet_photos]):
+        for path in tqdm(paths, desc='processing data'):
+            emd = transform(Image.open(path)).unsqueeze(dim=0)
+            with torch.no_grad():
+                embeds.append(triplet_model(emd.cuda()).squeeze(dim=0).cpu())
 
-    embeds = np.concatenate((sketches, photos), axis=0)
+    norm_gan_sketches, norm_gan_photos = [], []
+    for paths, embeds in zip([sketch_gan_paths, photo_gan_paths], [norm_gan_sketches, norm_gan_photos]):
+        for path in tqdm(paths, desc='processing data'):
+            emd = transform(Image.open(path)).unsqueeze(dim=0)
+            with torch.no_grad():
+                embeds.append(norm_gan_model(emd.cuda()).squeeze(dim=0).cpu())
+
+    triplet_gan_sketches, triplet_gan_photos = [], []
+    for paths, embeds in zip([sketch_gan_paths, photo_gan_paths], [triplet_gan_sketches, triplet_gan_photos]):
+        for path in tqdm(paths, desc='processing data'):
+            emd = transform(Image.open(path)).unsqueeze(dim=0)
+            with torch.no_grad():
+                embeds.append(triplet_gan_model(emd.cuda()).squeeze(dim=0).cpu())
+
+    norm_sketches = torch.stack(norm_sketches)
+    norm_photos = torch.stack(norm_photos)
+    triplet_sketches = torch.stack(triplet_sketches)
+    triplet_photos = torch.stack(triplet_photos)
+    norm_gan_sketches = torch.stack(norm_gan_sketches)
+    norm_gan_photos = torch.stack(norm_gan_photos)
+    triplet_gan_sketches = torch.stack(triplet_gan_sketches)
+    triplet_gan_photos = torch.stack(triplet_gan_photos)
+
+    norm_sketches = tsne.fit_transform(norm_sketches.numpy())
+    norm_photos = tsne.fit_transform(norm_photos.numpy())
+    triplet_sketches = tsne.fit_transform(triplet_sketches.numpy())
+    triplet_photos = tsne.fit_transform(triplet_photos.numpy())
+    norm_gan_sketches = tsne.fit_transform(norm_gan_sketches.numpy())
+    norm_gan_photos = tsne.fit_transform(norm_gan_photos.numpy())
+    triplet_gan_sketches = tsne.fit_transform(triplet_gan_sketches.numpy())
+    triplet_gan_photos = tsne.fit_transform(triplet_gan_photos.numpy())
+
+    norm_embeds = np.concatenate((norm_sketches, norm_photos), axis=0)
+    triplet_embeds = np.concatenate((triplet_sketches, triplet_photos), axis=0)
+    norm_gan_embeds = np.concatenate((norm_gan_sketches, norm_gan_photos), axis=0)
+    triplet_gan_embeds = np.concatenate((triplet_gan_sketches, triplet_gan_photos), axis=0)
+
     labels = sketch_labels + photo_labels
+    gan_labels = sketch_gan_labels + photo_gan_labels
     styles = ['sketch'] * num_class * num_sample + ['photo'] * num_class * num_sample
-    draw_fig(embeds, labels, '{}/{}_emb.pdf'.format(save_root, data_name), styles)
+
+    fig, axes = plt.subplots(1, 4, figsize=(22, 4))
+    axes[0].set_title(r'$\mathcal{L}_{triplet}$ without synthesize')
+    axes[1].set_title(r'$\mathcal{L}_{triplet}$ with synthesize')
+    axes[2].set_title(r'$\mathcal{L}_{norm}$ without synthesize')
+    axes[3].set_title(r'$\mathcal{L}_{norm}$ with synthesize')
+
+    draw_fig(triplet_embeds, labels, styles, axes[0])
+    draw_fig(triplet_gan_embeds, gan_labels, styles, axes[1])
+    draw_fig(norm_embeds, labels, styles, axes[2])
+    draw_fig(norm_gan_embeds, gan_labels, styles, axes[3], legend_on=True)
+    plt.savefig('{}/{}_emb.pdf'.format(save_root, data_name), bbox_inches='tight', pad_inches=0.1)
