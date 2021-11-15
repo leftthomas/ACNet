4727edff5eb70935570f5be2108df655e5b5ecd6
leftthomas <leftthomas@qq.com>
2021-11-16 01:09:50 +0800

    finish draw the dist of positive and negative

--- cls.py
+++ cls.py
@@ -41,14 +41,31 @@ if __name__ == '__main__':
     new_sketches = np.stack(new_sketches)
     photos = np.stack(photos)
 
-    op_distance = (np.abs(old_sketches - photos)).mean(axis=-1)
-    np_distance = (np.abs(new_sketches - photos)).mean(axis=-1)
+    positive_wo_distance = (np.abs(old_sketches - photos)).mean(axis=-1)
+    positive_w_distance = (np.abs(new_sketches - photos)).mean(axis=-1)
+
+    # construct negative pairs
+    photos = np.split(photos, len(photos) // num_sample, axis=0)
+    indexes, ref = list(range(0, len(photos))), list(range(0, len(photos)))
+    while np.any(np.array(indexes) == np.array(ref)):
+        random.shuffle(indexes)
+    photos = np.concatenate([photos[i] for i in indexes], axis=0)
+
+    negative_wo_distance = (np.abs(old_sketches - photos)).mean(axis=-1)
+    negative_w_distance = (np.abs(new_sketches - photos)).mean(axis=-1)
 
     sns.set()
-    fig, axes = plt.subplots(1, 2, figsize=(10, 5))
-    axes[0].set_title('w/o synthesis')
-    data = {'w/o synthesis': op_distance, 'w/ synthesis': np_distance}
-    ax = sns.histplot(pd.DataFrame(data), palette=['g', 'b'])
-    ax.set_xlabel('Distance')
-    ax.set_ylabel('# pairs')
-    plt.savefig('{}/{}_dist.pdf'.format(save_root, data_name), bbox_inches='tight', pad_inches=0.1)
+    fig, axes = plt.subplots(1, 2, figsize=(12, 5))
+    axes[0].set_title('w/o synthesis', fontsize=18)
+    axes[1].set_title('w/ synthesis', fontsize=18)
+    data = {'positive': positive_wo_distance, 'negative': negative_wo_distance}
+    sns.histplot(pd.DataFrame(data), palette=['g', 'b'], ax=axes[0], binrange=(0, 250))
+    data = {'positive': positive_w_distance, 'negative': negative_w_distance}
+    sns.histplot(pd.DataFrame(data), palette=['g', 'b'], ax=axes[1], binrange=(0, 250))
+    axes[0].set_xlabel('Distance')
+    axes[0].set_ylabel('# pairs')
+    axes[1].set_xlabel('Distance')
+    axes[1].set_ylabel('# pairs')
+    axes[0].set_ylim(0, 350)
+    axes[1].set_ylim(0, 350)
+    plt.savefig('{}/{}_pn.pdf'.format(save_root, data_name), bbox_inches='tight', pad_inches=0.1)
