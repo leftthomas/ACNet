d43c702daace6c89f787e49e2699ba73f197afff
leftthomas <leftthomas@qq.com>
2021-11-16 14:54:57 +0800

    add binary vector computer; remove vis emb code

--- binary.py
+++ binary.py
@@ -2,9 +2,9 @@ import torch
 
 from utils import DomainDataset, compute_metric
 
-vectors = torch.load('result/all/tuberlin_resnet50_512_vectors.pth')
+vectors = torch.load('result/all/sketchy_resnet50_512_vectors.pth')
 vectors[vectors > 0] = 1.0
 vectors[vectors < 0] = 0.0
-val_data = DomainDataset('/data', 'tuberlin', split='val')
+val_data = DomainDataset('/data', 'sketchy', split='val')
 result = compute_metric(vectors, torch.tensor(val_data.domains), torch.tensor(val_data.labels))
 print(result)
